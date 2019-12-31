876a3953e63cdc63c1fe872da95b49ef580a80f6
Christian Gewert <cgewert@gmail.com>
2019-12-31 02:13:47 +0100

    add customized linting rules for typescript, move electron to dependencies

--- main.ts
+++ main.ts
@@ -1,10 +1,7 @@
 import { app } from "electron";
-import fs from "fs";
 import { Editor } from "./src/tshedit";
 
 app.on("ready", () => {
   const tshedit = new Editor();
-  const root = fs.readFileSync("./index.html");
-  console.log(root);
   tshedit.createWindow(720, 1280);
 });
--- src/tshedit.ts
+++ src/tshedit.ts
@@ -1,6 +1,7 @@
 import {BrowserWindow} from "electron";
 
 export class Editor {
+
     public createWindow(h: number, w: number) {
         const win = new BrowserWindow({
             height: h,
