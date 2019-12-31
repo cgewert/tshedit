89cc6034dbcef9fb07bf328037cfe7591fe6d1bb
Christian Gewert <cgewert@gmail.com>
2019-12-31 04:38:32 +0100

    add renderer.ts, add lintint rule for console log, import renderer.js in index.html

--- src/tshedit.ts
+++ src/tshedit.ts
@@ -1,8 +1,8 @@
-import {BrowserWindow} from "electron";
+import { BrowserWindow } from "electron";
 
 export class Editor {
 
-    public createWindow(h: number, w: number) {
+    public createWindow(h: number, w: number): void {
         const win = new BrowserWindow({
             height: h,
             webPreferences: {
@@ -11,6 +11,10 @@ export class Editor {
             width: w,
         });
 
-        win.loadFile("index.html");
+        // win.loadFile("index.html");
+        win.loadFile("index.html")
+            .then((result: void): void => {
+                win.webContents.openDevTools({ mode: "right", });
+            });
     }
 }
